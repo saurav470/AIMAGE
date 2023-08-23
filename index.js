7a31f9cd6bceb0d2c1088d188fc211e2f265b6d0
saurav sharma <sauravsharma@sauravs-MacBook-Air.local>
2023-08-23 15:26:26 +0530

    AIIMAGE 1

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import * as path from 'path';
+
+import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
+const __dirname = path.resolve();
+const PORT = process.env.PORT || 8080
+console.log(__dirname);
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, "build")))
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
+app.use("*", (req, res) => {
+  return res.sendFile(path.resolve(__dirname, "build", "index.html"))
+})
+
+app.get('/', async (req, res) => {
+  res.status(200).json({
+    message: 'Hello from DALL.E!',
+  });
+});
+
+const startServer = async () => {
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
